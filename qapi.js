c7b27b92682b785d8c4b6b72f679162f52a9ea0f
Alexander <webisforce@gmail.com>
2021-01-27 13:51:53 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,41 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6011457ad34e73001fe670a5",
+				"6011457ad34e73001fe670a7",
+				"6011458eaae0d1001eea8fa4",
+				"60114593ae0c05001e87b69d"
+			]
+		},
+		"6011457ad34e73001fe670a5": {
+			"id": "6011457ad34e73001fe670a5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6011457ad34e73001fe670a7": {
+			"id": "6011457ad34e73001fe670a7",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"6011458eaae0d1001eea8fa4": {
+			"id": "6011458eaae0d1001eea8fa4",
+			"pageUrl": "testen",
+			"name": "test.en"
+		},
+		"60114593ae0c05001e87b69d": {
+			"id": "60114593ae0c05001e87b69d",
+			"pageUrl": "testru",
+			"name": "test.ru"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
